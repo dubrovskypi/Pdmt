eccc7b432b1f5c9508ab48457887a57a392ba4a2
dubrovskypi <dubrovsky.pi@gmail.com>
2026-04-29 23:50:56 +0300

    style: apply design tokens light theme; chore: remove dead OpenSans font ref

--- Pdmt.Maui/MauiProgram.cs
+++ Pdmt.Maui/MauiProgram.cs
@@ -17,10 +17,7 @@ public static class MauiProgram
         builder
             .UseMauiApp<App>()
             .UseMauiCommunityToolkit()
-            .ConfigureFonts(fonts =>
-            {
-                fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
-            })
+            .ConfigureFonts(fonts => { })
             .ConfigureMauiHandlers(handlers =>
             {
 #if ANDROID
