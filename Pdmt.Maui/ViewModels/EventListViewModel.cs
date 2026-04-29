d61681c1fe163c47f38bfea72330fd85328baba4
dubrovskypi <dubrovsky.pi@gmail.com>
2026-04-30 01:06:04 +0300

    feat(maui): redesign event list (filter panel + extended FAB) and event form (sticky save bar)

--- Pdmt.Maui/ViewModels/EventListViewModel.cs
+++ Pdmt.Maui/ViewModels/EventListViewModel.cs
@@ -23,17 +23,26 @@ public partial class EventListViewModel(
 
     public ObservableCollection<TagFilter> TagFilters { get; } = [];
 
+    private bool _filtersInitialized;
+
     [ObservableProperty]
+    [NotifyPropertyChangedFor(nameof(HasActiveFilters))]
     private DateTime? _filterFrom;
 
     [ObservableProperty]
+    [NotifyPropertyChangedFor(nameof(HasActiveFilters))]
     private DateTime? _filterTo;
 
     [ObservableProperty]
+    [NotifyPropertyChangedFor(nameof(HasActiveFilters))]
     private EventTypeFilter _selectedTypeFilter = new("All", null);
 
     [ObservableProperty]
+    [NotifyPropertyChangedFor(nameof(HasActiveFilters))]
     private TagFilter? _selectedTagFilter;
+
+    [ObservableProperty]
+    private bool _isFilterPanelVisible;
 
     [ObservableProperty]
     private bool _isRefreshing;
@@ -43,6 +52,45 @@ public partial class EventListViewModel(
 
     [ObservableProperty]
     private string? _errorMessage;
+
+    public bool HasActiveFilters =>
+        SelectedTypeFilter.Value is not null
+        || SelectedTagFilter is not null
+        || (FilterFrom.HasValue && FilterFrom.Value.Date != DateTime.Today.AddDays(-6))
+        || (FilterTo.HasValue && FilterTo.Value.Date != DateTime.Today.AddDays(1));
+
+    public bool IsAllTypeSelected => SelectedTypeFilter.Value is null;
+    public bool IsPositiveTypeSelected => SelectedTypeFilter.Value == EventType.Positive;
+    public bool IsNegativeTypeSelected => SelectedTypeFilter.Value == EventType.Negative;
+
+    [RelayCommand]
+    private void SelectTypeFilter(EventTypeFilter filter) => SelectedTypeFilter = filter;
+
+    partial void OnSelectedTypeFilterChanged(EventTypeFilter value)
+    {
+        OnPropertyChanged(nameof(IsAllTypeSelected));
+        OnPropertyChanged(nameof(IsPositiveTypeSelected));
+        OnPropertyChanged(nameof(IsNegativeTypeSelected));
+        if (_filtersInitialized) _ = ApplyFiltersAsync();
+    }
+
+    partial void OnSelectedTagFilterChanged(TagFilter? value)
+    {
+        if (_filtersInitialized) _ = ApplyFiltersAsync();
+    }
+
+    partial void OnFilterFromChanged(DateTime? value)
+    {
+        if (_filtersInitialized) _ = ApplyFiltersAsync();
+    }
+
+    partial void OnFilterToChanged(DateTime? value)
+    {
+        if (_filtersInitialized) _ = ApplyFiltersAsync();
+    }
+
+    [RelayCommand]
+    private void ToggleFilterPanel() => IsFilterPanelVisible = !IsFilterPanelVisible;
 
     private void SetDefaultDateRange()
     {
@@ -90,6 +138,7 @@ public partial class EventListViewModel(
             }
 
             await FetchAndPopulateAsync();
+            _filtersInitialized = true;
         }
         catch
         {
@@ -142,9 +191,11 @@ public partial class EventListViewModel(
     [RelayCommand]
     private async Task ResetFiltersAsync()
     {
+        _filtersInitialized = false;
         SetDefaultDateRange();
         SelectedTypeFilter = EventTypeFilters[0];
         SelectedTagFilter = null;
+        _filtersInitialized = true;
         IsBusy = true;
         ErrorMessage = null;
         try
